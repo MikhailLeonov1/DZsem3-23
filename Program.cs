ebb77d403c6d883a6cc41bea4d9ee795e16be1a4
user name <you@example.com>
2023-01-22 18:08:58 +0300

    дз номер 23

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int cube = ReadInt("Введите N: ");  // Ввод N
+
+
+for (int i = 1; i <= cube; i++)  // Цикл
+
+{
+    Console.Write($"{i * i * i} "); // Вывод кубов чисел
+}
+
+int ReadInt(string message) // Функция для ввода числа
+
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
